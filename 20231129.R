c732d400f9371b253bd02f1eedc0527a8e789a85
le402n40\uer <ccannlin@gmail.com>
2023-11-29 17:46:25 +0800

    練習2 自己手刻圖

--- 20231129.R
+++ 20231129.R
@@ -5,3 +5,20 @@ hist(dat[,"eGFR"])
 boxplot(dat[,"eGFR"])
 pie(table(dat[,"Education"]))
 barplot(table(dat[,"Education"]))
+
+m0 = 33.2
+s0 = 6.4/sqrt(sum(dat[,"Disease"] == 0, na.rm = TRUE))
+m1 = 34.4
+s1 = 7.2/sqrt(sum(dat[,"Disease"] == 1, na.rm = TRUE))
+
+XXX = c(m0, m1)
+barplot(XXX, col = c("gray50", "white"), xlab = "Disease", ylab = "eGFR", ylim = c(0, 43))
+lines(c(1.9, 1.9), c(m1 - qnorm(0.975) * s1, m1 + qnorm(0.975) * s1), lwd = 3)
+lines(c(1.75, 2.05), c(m1 + qnorm(0.975) * s1, m1 + qnorm(0.975) * s1), lwd = 3)
+lines(c(1.75, 2.05), c(m1 - qnorm(0.975) * s1, m1 - qnorm(0.975) * s1), lwd = 3)
+lines(c(0.7, 0.7), c(m0 - qnorm(0.975) * s0, m0 + qnorm(0.975) * s0), lwd = 3)
+lines(c(0.55, 0.85), c(m0 + qnorm(0.975) * s0, m0 + qnorm(0.975) * s0), lwd = 3)
+lines(c(0.55, 0.85), c(m0 - qnorm(0.975) * s0, m0 - qnorm(0.975) * s0), lwd = 3)
+lines(c(0.7, 0.7, 1.9, 1.9), c(36, 38, 38, 36), lwd = 3)
+text(1.3, 40, "p = 0.020")
+legend("topright", c("Control", "Case"), fill = c("gray50", "white"))
